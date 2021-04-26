61c870dd51020620ca8eeb3db1db7f647fe81390
Rafael Benzaquem Neto <rafael.neto@outlook.com>
2021-04-26 10:12:50 -0300

    setup do projeto

---
+++ src/test/java/br/com/zup/academy/benzaquem/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.academy.benzaquem.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
